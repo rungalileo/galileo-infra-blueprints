13cc1ea05bc4c2bbe1d41deb74fb2fce2c748aa8
Tyler Ashworth <tyler@rungalileo.io>
2023-12-13 16:26:37 -0600

    Add networking setup to AWS example

--- terraform/modules/galileo-eks/exampels/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "vpc_id" {
-  type        = string
-  description = "ID of the VPC where the cluster and its nodes will be provisioned"
-}
-
-variable "private_subnet_id" {
-  type        = list(string)
-  description = "A list of subnet IDs where the nodes/node groups will be provisioned"
-}
---
+++ terraform/modules/galileo-eks/examples/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = var.region
+}
+
+module "galileo" {
+  source = "../."
+
+  create_kms_key            = false
+  cluster_encryption_config = []
+
+  vpc_id            = aws_vpc.vpc.id
+  private_subnet_id = [
+    aws_subnet.public_subnet_a.id,
+    aws_subnet.private_subnet_a.id, 
+    aws_subnet.public_subnet_b.id, 
+    aws_subnet.private_subnet_b.id, 
+    aws_subnet.public_subnet_c.id,
+    aws_subnet.private_subnet_c.id
+  ]
+}
+
